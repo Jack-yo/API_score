e84e350b4c77fdfb533d11f10a4bb215b6999901
Yjj666 <1257177646@qq.com>
2019-03-26 22:06:56 +0800

    demo.js

--- demo.js
+++ demo.js
@@ -7,7 +7,7 @@ const mysql  = require('mysql');
 var connection = mysql.createConnection({     
   host     : 'localhost',       
   user     : 'root',              
-  password : '19841029Qq',       
+  password : 'password',       
   port: '3306',                   
   database: 'demo' 
 }); 
